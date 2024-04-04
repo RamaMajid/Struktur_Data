579899ecdf14aa4506271e051466238a840cfa51
Rama Majid <127276782+RamaMajid@users.noreply.github.com>
2024-04-04 09:22:52 +0700

    Delete src/Grafkom directory

--- src/Grafkom/Main.java
+++
@@ -1,9 +0,0 @@
-package Grafkom;
-
-public class Main {
-    public static void main(String[] args) {
-        Point A = new Point(0, 0);
-        Point B = new Point(10, 21);
-        DDA.drawLine(A, B);
-    }
-}
